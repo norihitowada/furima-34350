e502143def8f341c37521f7b277d5748f09bedca
和田憲人 <norihito.wada@gmail.com>
2021-05-27 16:45:19 +0900

    usersテーブルの作成

--- db/migrate/20210527072458_devise_create_users.rb
+++ db/migrate/20210527072458_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name_kanji_last,    null: false
+      t.string :name_kanji_first,   null: false
+      t.string :name_kana_last,     null: false
+      t.string :name_kana_first,    null: false
+      t.date   :birthday,           null: false
+
 
       ## Recoverable
       t.string   :reset_password_token

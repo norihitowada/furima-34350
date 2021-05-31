5fa44a761664e2c0fe2d29fc0c080ec62a364aff
和田憲人 <norihito.wada@gmail.com>
2021-05-31 19:09:26 +0900

    ユーザー登録のテストコード作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :name_kanji_last, :name_kanji_first, :name_kana_last, :name_kana_first, :birthday])
+  end
+
 
   def index
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :name_kanji_last, presence: true
+  validates :name_kanji_first, presence: true
+  validates :name_kana_last, presence: true
+  validates :name_kana_first, presence: true
+  validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :name_kanji_last
+    validates :name_kanji_first
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'カタカナを使用してください' } do
+    validates :name_kana_last
+    validates :name_kana_first
+  end
+
+
 end
+

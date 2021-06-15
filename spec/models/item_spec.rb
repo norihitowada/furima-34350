923b616fa61302983888442db5282984251c7073
和田憲人 <norihito.wada@gmail.com>
2021-06-15 23:26:31 +0900

    修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,27 +16,27 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it 'カテゴリーはidが1以外で登録できる' do
-        @item.category_id = '5'
+        @item.category_id = 5
         expect(@item).to be_valid
       end
       it '商品の状態はidが１以外で登録できる' do
-        @item.condition_id = '3'
+        @item.condition_id = 3
         expect(@item).to be_valid
       end
       it '配送料の負担はidが１以外で登録できる' do
-        @item.delivery_cost_id = '3'
+        @item.delivery_cost_id = 3
         expect(@item).to be_valid
       end
       it '発送元の地域はidが１以外で登録できる' do
-        @item.prefecture_id = '3'
+        @item.prefecture_id = 3
         expect(@item).to be_valid
       end
       it '発送までの日数はidが１以外で登録できる' do
-        @item.day_id = '3'
+        @item.day_id = 3
         expect(@item).to be_valid
       end
       it '販売価格はidが300から9,999,999の範囲で登録できる' do
-        @item.prefecture_id = '10000'
+        @item.prefecture_id = 10000
         expect(@item).to be_valid
       end
       context '出品できないとき' do
@@ -51,27 +51,27 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Explanation can't be blank")
         end
         it 'カテゴリーが1では登録できない' do
-          @item.category_id = '1'
+          @item.category_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Category must be other than 1')
         end
         it '商品説明が1では登録できない' do
-          @item.condition_id = '1'
+          @item.condition_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Condition must be other than 1')
         end
         it '配送料負担が1では登録できない' do
-          @item.delivery_cost_id = '1'
+          @item.delivery_cost_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Delivery cost must be other than 1')
         end
         it '発送元の地域が1では登録できない' do
-          @item.prefecture_id = '1'
+          @item.prefecture_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
         end
         it '発送までの日数が1では登録できない' do
-          @item.day_id = '1'
+          @item.day_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Day must be other than 1')
         end
@@ -86,15 +86,33 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include('Price is not a number')
         end
         it '販売価格が300未満では登録できない' do
-          @item.price = '299'
+          @item.price = 299
           @item.valid?
           expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
         end
         it '販売価格が10,000,000以上では登録できない' do
-          @item.price = '10000000'
+          @item.price = 10000000
           @item.valid?
           expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
         end
+        it '販売価格が半角英数字混合では登録できない' do
+          @item.price = '10a'
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price is not a number')
+        end
+        it '販売価格が半角英語だけでは登録できない' do
+          @item.price = 'aaa'
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price is not a number')
+        end
+        it 'userが紐づいていないと登録できない' do
+          @item.user = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include('User must exist')
+        end
+
+
+
       end
     end
   end
